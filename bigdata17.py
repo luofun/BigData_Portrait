f279b966a51e67bbcd7d36a129318d5d15ba4a1f
luofun <627018553@qq.com>
2017-09-26 00:14:20 +0800

    add 17

---
+++ bigdata17.py
@@ -0,0 +1,16 @@
+﻿
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+chrome=webdriver.Chrome()
+url='http://www.phei.com.cn/'
+chrome.get('http://www.phei.com.cn/')
+'''assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("selenium")
+elem.send_keys(Keys.RETURN)'''
+time.sleep(10)
+with open('phei.html','w',encoding='utf8') as f:
+    f.write(chrome.page_source)
+chrome.quit()
