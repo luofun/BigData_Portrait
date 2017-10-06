1a0ce6b56fcaee7ebd0346781054c1374674cce1
luofun <627018553@qq.com>
2017-10-06 11:29:40 +0800

    updata 1

--- pybasicreview/1/pystudy1.py
+++ pybasicreview/1/pystudy1.py
@@ -1,4 +1,4 @@
-filename='aaa'
+﻿filename='aaa'
 x1='enddb.'
 x2='endrec.'
 x3='=>'
@@ -30,6 +30,26 @@ def load(name=filename):
         key=input()
     return db
 
+class Person:
+    def __init__(self, name, age, pay=0, job=None):
+        self.name=name
+        self.age=age
+        self.pay=pay
+        self.job=job
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self,percent):
+        self.pay*=(1.0+percent)
+    def __str__(self):
+        return '<%s => %s: %s, %s>'%(self.__class__.__name__,self.name,self.job,self.pay)
+
+class Manager(Person):
+    def __init__(self,name,age,pay):
+        Person.__init__(self,name,age,pay,'manager')
+    def giveRaise(self, percent, bonus=0.1):
+        self.pay*=(1.0+percent+bonus)
+        
+
 if __name__=="__main__":
     pass
     filex=open('aaaa','w')
@@ -37,6 +57,7 @@ if __name__=="__main__":
     filex.close()
     filey=open('aaaa')
     import sys
+    concon=sys.stdin
     sys.stdin=filey
     key=input()
     print("input:"+ key + '\n')
@@ -54,4 +75,109 @@ if __name__=="__main__":
         print(key,'=>\n',db[key])
     print(db['sue']['name'])
     file333.close()
-    
+    print('-'*30)
+    from initdata import bob,sue,tom
+    for(key, record) in [('bob',bob),('sue',sue),('tom',tom)]:
+        recfile=open(key+'.pkl','wb')
+        pickle.dump(record,recfile)
+        recfile.close()
+    print('-'*30)
+    import glob
+    for filename in glob.glob('*.pkl'):
+        recfile=open(filename,'rb')
+        record=pickle.load(recfile)
+        print(filename,'=>\n',record);
+    suefile=open('sue.pkl','rb')
+    print(pickle.load(suefile)['name'])
+    print('-'*30)
+    suefile=open('sue.pkl','rb')
+    sue=pickle.load(suefile)
+    suefile.close()
+    sue['age']*=1.10
+    suefile=open('sue.pkl','wb')
+    pickle.dump(sue,suefile)
+    suefile.close()
+    import shelve
+    db=shelve.open('people-shelve')
+    db['bob']=bob
+    db['sue']=sue
+    db.close()
+    db=shelve.open('people-shelve')
+    for key in db:
+        print(key,'=>\n',db[key])
+    print(db['sue']['age'])
+    db.close()
+    print('-'*30)
+    db=shelve.open('people-shelve')
+    sue=db['sue']
+    sue['age']*=1.50
+    db['sue']=sue
+    db['tom']=tom
+    db.close()
+    print('-'*30)
+    db=shelve.open('people-shelve')
+    print(db['sue']['age'])
+    db.close()
+    print('-'*30)
+    bob=Person('Bob Smith',42,30000,'software')
+    sue=Person('Sue Jones',45,40000,'hardware')
+    print(bob.name,sue.pay)
+    print(bob.name.split()[-1])
+    sue.pay*=1.10
+    print(sue.pay)
+    print('-'*30)
+    print(bob.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
+    print('-'*30)
+    tom=Manager(name='tom doe',age=50,pay=50000)
+    print(tom.lastName())
+    tom.giveRaise(.20)
+    print(tom.pay)
+    print('-'*30)
+    tom=Manager('Tom Jones',50,40000)
+    print(tom)
+    bob=Person('Bob Smith',44)
+    sue=Person('Sue Jones',47,40000,'hardware')
+    tom=Manager(name='Tom Doe',age=50,pay=50000)
+    print(sue,sue.pay,sue.lastName())
+    for obj in  (bob,sue,tom):
+        obj.giveRaise(.10)
+        print(obj)
+    print('-'*30)
+    db=shelve.open('class-shelve')
+    db['bob']=bob
+    db['sue']=sue
+    db['tom']=tom
+    db.close()
+    print('-'*30)
+    db=shelve.open('class-shelve')
+    for key in db:
+        print(key,'=>\n',db[key].name,db[key].pay)
+    bob=db['bob']
+    print(bob.lastName())
+    print(db['tom'].lastName())
+    print('-'*30)
+    db=shelve.open('class-shelve')
+    sue=db['sue']
+    sue.giveRaise(.25)
+    db['sue']=sue
+    tom=db['tom']
+    tom.giveRaise(.20)
+    db['tom']=tom
+    db.close()
+    print('-'*30)
+    newtext='ttt'
+    record=Person(name='?',age='?')
+    field='name'
+    print(record)
+    sys.stdin=concon
+    print(concon)
+    newtext=input('input:')
+    setattr(record,field,newtext)
+    #record.name=newtext
+    print(record)
+    print('-'*30)
+
+
+
